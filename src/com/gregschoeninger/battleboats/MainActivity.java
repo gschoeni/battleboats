ca46cb47971fc3052e2cc23f97605fd76d634346
Andrew Doppler <adoppler@mymail.mines.edu>
2013-01-06 11:14:09 -0800

    Making main screen fullscreen

--- src/com/gregschoeninger/battleboats/MainActivity.java
+++ src/com/gregschoeninger/battleboats/MainActivity.java
@@ -18,6 +18,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -26,6 +28,8 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     	requestAccessToken();
     }
